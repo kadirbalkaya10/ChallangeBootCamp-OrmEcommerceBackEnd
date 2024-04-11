7659036ae9d1a459501845c6ab9a14cacda0573a
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-04-11 00:09:41 -0400

    Added, Tag Route for update tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,8 +44,24 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updatedTag);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 router.delete("/:id", (req, res) => {
