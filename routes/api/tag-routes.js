f1a7c41013b7cd5f218590b9e00a159657062b52
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-04-11 00:11:14 -0400

    Added, Tag Route for destroy tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,8 +64,18 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const destroyedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(destroyedTag);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
